36bf37b56458a1dd33e825189ecb632a77434c34
Nitin Dixit <55051669+nitin-dixit@users.noreply.github.com>
2026-06-02 17:55:00 +0530

    feat(snippets): add snippet model

---
+++ internal/models/snippets.go
@@ -0,0 +1,83 @@
+package models
+
+import (
+	"database/sql"
+	"errors"
+	"time"
+)
+
+type Snippet struct {
+	ID      int
+	Title   string
+	Content string
+	Created time.Time
+	Expires time.Time
+}
+
+type SnippetModel struct {
+	DB *sql.DB
+}
+
+func (m *SnippetModel) Insert(title string, content string, expires int) (int, error) {
+	query := `insert into snippets (title, content, expires)
+	values ($1,$2, current_timestamp + ($3 * interval '1 day'))
+	returning id`
+
+	var id int
+
+	err := m.DB.QueryRow(query, title, content, expires).Scan(&id)
+	if err != nil {
+		return 0, err
+	}
+
+	return id, nil
+}
+
+func (m *SnippetModel) Get(id int) (Snippet, error) {
+	query := `select id, title, content, created, expires from snippets
+	where expires > current_timestamp and id=$1`
+
+	row := m.DB.QueryRow(query, id)
+
+	var s Snippet
+	err := row.Scan(&s.ID, &s.Title, &s.Content, &s.Created, &s.Expires)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return Snippet{}, ErrNoRecord
+		} else {
+			return Snippet{}, err
+		}
+	}
+	return s, nil
+}
+
+func (m *SnippetModel) Latest() ([]Snippet, error) {
+	query := `select id,title,content,created,expires from snippets
+	where expires > current_timestamp order by id desc limit 10`
+
+	rows, err := m.DB.Query(query)
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close()
+
+	var snippets []Snippet
+
+	for rows.Next() {
+		var s Snippet
+
+		err := rows.Scan(&s.ID, &s.Title, &s.Content, &s.Created, &s.Expires)
+		if err != nil {
+			return nil, err
+		}
+
+		snippets = append(snippets, s)
+
+	}
+
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+	return snippets, nil
+}
