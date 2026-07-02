13e851600078cc196a81a15b00e025d873ddda35
Nitin Dixit <55051669+nitin-dixit@users.noreply.github.com>
2026-07-02 12:07:47 +0530

    feat(security): add CORF protection using http.CrossOriginProtection

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -12,7 +12,7 @@ func (app *application) routes() http.Handler {
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
 	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
 
-	dynamic := middleware.New(app.sessionManager.LoadAndSave)
+	dynamic := middleware.New(app.sessionManager.LoadAndSave, middleware.PreventCORF)
 
 	mux.Handle("GET /{$}", dynamic.ThenFunc(app.home))
 	mux.Handle("GET /snippet/view/{id}", dynamic.ThenFunc(app.snippetView))
