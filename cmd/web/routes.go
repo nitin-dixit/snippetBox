1183d6eb1ceb6da6323644848fc0e7d556626c42
Nitin Dixit <55051669+nitin-dixit@users.noreply.github.com>
2026-07-04 13:49:56 +0530

    feat(tests): implement end to end testing using httptest.Server

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -11,6 +11,7 @@ func (app *application) routes() http.Handler {
 	mux := http.NewServeMux()
 
 	mux.Handle("GET /static/", http.FileServerFS(ui.Files))
+	mux.HandleFunc("GET /ping", ping)
 
 	dynamic := middleware.New(app.sessionManager.LoadAndSave, middleware.PreventCORF, app.authenticate)
 
